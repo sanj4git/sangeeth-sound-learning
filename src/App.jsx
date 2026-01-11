9cc92a1ff02d4f762f709b1c903805c46e9976a9
sanj4git <arsanjay0906@gmail.com>
2026-01-11 20:16:05 +0530

    Phase 2 : Gloal UI and branding

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./components/Layout";
+
+import Home from "./pages/Home";
+import Info from "./pages/Info";
+import Learn from "./pages/Learn";
+import Play from "./pages/Play";
+
 function App(){
-  return (
-    <div>
-      Sangeeth
-    </div>
-  );
+
+  return(
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/info" element={<Info />}/>
+          <Route path="/learn" element={<Learn/>}/>
+          <Route path="/play" element={<Play/>}/>
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  )
+
 }
 
 export default App;
